9415464717f6c6195bb3948bbf7e100193d10d58
Marvel Mathew <m@marvelmathew.ca>
2015-09-18 21:57:34 -0500

    Dead-code elimination

--- src/main/scala/checker/Checker.scala
+++ src/main/scala/checker/Checker.scala
@@ -1,8 +1,11 @@
 package checker
 
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
 
+@JSExport
 object Checker extends JSApp {
+  @JSExport
   def main() {
     println("Hello world")
   }
